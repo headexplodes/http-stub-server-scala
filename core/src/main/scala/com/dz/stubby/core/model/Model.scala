355c7527906d8636aab404c69d9f9c553e31753d
Robert Parsons <headexplodes@gmail.com>
2013-08-27 16:32:32 +1000

    Fixed issue with JSON serialization

--- core/src/main/scala/com/dz/stubby/core/model/Model.scala
+++ core/src/main/scala/com/dz/stubby/core/model/Model.scala
@@ -4,14 +4,15 @@ case class StubParam(
   name: String,
   value: String)
 
-abstract class StubMessage(
-    val headers: List[StubParam],
-    val body: Option[AnyRef]) {
-  
-  type T <: StubMessage 
+trait StubMessage {
+
+  type T <: StubMessage
+
+  val headers: List[StubParam]
+  val body: Option[AnyRef]
 
   def copyWith(headers: List[StubParam] = headers, body: Option[AnyRef] = body): T
-  
+
   def getHeader(name: String): Option[String] =
     headers.find(_.name.equalsIgnoreCase(name)).map(_.value)
   def getHeaders(name: String): Seq[String] =
@@ -21,20 +22,20 @@ abstract class StubMessage(
     copyWith(headers :+ StubParam(name, value))
   def removeHeader(name: String): T =
     copyWith(headers.filterNot(_.name.equalsIgnoreCase(name)))
-  def setHeader(name: String, value: String): T#T = 
+  def setHeader(name: String, value: String): T#T =
     removeHeader(name).addHeader(name, value)
-    
+
 }
 
 case class StubRequest(
     val method: Option[String] = None, // optional so we can create filters
     val path: Option[String] = None,
     val params: List[StubParam] = Nil,
-    override val headers: List[StubParam] = Nil,
-    override val body: Option[AnyRef] = None) extends StubMessage(headers, body) {
+    val headers: List[StubParam] = Nil,
+    val body: Option[AnyRef] = None) extends StubMessage {
 
   type T = StubRequest
-  
+
   def getParam(name: String): Option[String] =
     params.find(_.name.equalsIgnoreCase(name)).map(_.value)
   def getParams(name: String): Seq[String] =
@@ -50,11 +51,11 @@ case class StubRequest(
 
 case class StubResponse(
     val status: Int,
-    override val headers: List[StubParam] = Nil,
-    override val body: Option[AnyRef] = None) extends StubMessage(headers, body) {
+    val headers: List[StubParam] = Nil,
+    val body: Option[AnyRef] = None) extends StubMessage {
 
   type T = StubResponse
-  
+
   override def copyWith(headers: List[StubParam], body: Option[AnyRef]): StubResponse =
     copy(headers = headers, body = body)
 
